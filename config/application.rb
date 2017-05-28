da356167227abda8f8cf5187b3d3e6cba098cf61
Hayato IZUMI <h-izumi@9uelle.jp>
2017-05-28 14:22:09 +0900

    Configuration for i18n

--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,8 @@ module Skelton51
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.i18n.available_locales = [:en, :ja]
+    config.i18n.default_locale = :ja
   end
 end
